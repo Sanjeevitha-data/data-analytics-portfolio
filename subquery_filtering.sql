400fab186cc7a9ad08bde8486561873d6f3285d8
Sanjeevitha-data <sanrk74@gmail.com>
2026-07-28 16:39:08 +0530

    Delete subquery_filtering.sql

--- subquery_filtering.sql
+++
@@ -1,9 +0,0 @@
-SELECT film_id, title
-FROM film
-WHERE film_id IN
-(SELECT inventory.film_id  
-FROM rental
-INNER JOIN inventory 
-ON inventory.inventory_id = rental.inventory_id
-WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30')
-ORDER BY film_id;
