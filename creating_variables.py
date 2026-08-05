040f31bbda25b060c5394e59a4b8c9b8e430bb7b
Sanjeevitha-data <sanrk74@gmail.com>
2026-08-05 13:12:28 +0530

    Add files via upload

---
+++ creating_variables.py
@@ -0,0 +1,16 @@
+
+Create variables to store the following information: job id (int), company name(string), salary rate(string, but consider how you might store this as a numerical type for calc), job work from home(boolean)
+
+company_name = 'DataWiz Inc.'
+job_wfh = True
+job_id = 101
+job_title = 'Data Analyst'
+job_salary = 125000
+
+print("Data Job Description")
+print("********************")
+print("Job Id:      ", job_id)
+print("Company Name:", company_name)
+print("Job Title:   ", job_title)
+print("Salary:      ", job_salary)
+print("WFH:         ", job_wfh)
