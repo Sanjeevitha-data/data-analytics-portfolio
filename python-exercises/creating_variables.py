ece1443476f3384459542c7553b7114ab733d17d
Sanjeevitha-data <sanrk74@gmail.com>
2026-08-05 13:18:25 +0530

    Update creating_variables.py
    Updated comments for clarity and consistency in variable definitions.

--- python-exercises/creating_variables.py
+++ python-exercises/creating_variables.py
@@ -1,12 +1,16 @@
 
-Create variables to store the following information: job id (int), company name(string), salary rate(string, but consider how you might store this as a numerical type for calc), job work from home(boolean)
-
+#Create variables to store the following information: 
+job id (int),
+company name(string),
+salary rate(int),
+job work from home(boolean)
+***********************************
 company_name = 'DataWiz Inc.'
 job_wfh = True
 job_id = 101
 job_title = 'Data Analyst'
 job_salary = 125000
-
+***********************************
 print("Data Job Description")
 print("********************")
 print("Job Id:      ", job_id)
